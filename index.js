7be5ebd0baaac3a4ac3c704e40f1d447be465049
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-04-16 09:21:28 +0530

    Added the database with post request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
+app.use(express.json())
 
 app.get('/test', (req, res) => {
     res.json({
@@ -9,12 +10,12 @@ app.get('/test', (req, res) => {
     });
 });
 
-app.listen(5000, () => {
-    console.log("Server running");
-})
+// this line suggests only connect to db
+mongoose.connect('mongodb://127.0.0.1:27017/testDB')
+  .then(() => console.log('Connected to testDB!'));
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
-  .then(() => console.log('Connected!'));
+// using aync await to connect to db 
+// await mongoose.connect('mongodb://127.0.0.1/my_database');
 
 // In order to view this db, we have specify what kind of database it is, and what is the type of data it is going to store.
 // Schema of the db model
@@ -35,4 +36,19 @@ const UserSchema = new Schema({
   });
 
 const UserModel = mongoose.model('UserDatabase', UserSchema);
-  // this model will be used to interact with our db using our apis
+// this model will be used to interact with our db using our apis
+
+
+app.post("/register", (req, res) => {
+    const {username, email, password} = req.body;
+    console.log(username, email, password);
+    res.json({
+        message: "data received"
+    });
+});
+
+
+
+app.listen(5000, () => {
+    console.log("Server running");
+});
